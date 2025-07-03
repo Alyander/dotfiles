ac9ab6e8fbf0bd0f913dd188e2acd00569abedae
Alyander <momoroot33@gmail.com>
2025-07-03 12:21:02 +0300

    update

--- dwm-6.5/config.h
+++ dwm-6.5/config.h
@@ -35,8 +35,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	// { "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{"Thunar", NULL, NULL, 0, 1, -1},
+	{"pavucontrol", NULL, NULL, 0, 1, -1},
+	{"librewolf", NULL, "Библиотека", 0, 1, -1},
 };
 
 /* layout(s) */
--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-   set fish_greeting
-   alias v="vim"
-   alias nv="nvim"
-   set -gx PATH $PATH /home/anatolii/.config/nvim/lsps/lua_ls/bin
-end
-
-
