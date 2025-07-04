a8329a00b00f0db89a58eb8f835bc0209df1e680
Alyander <momoroot33@gmail.com>
2025-07-04 11:05:54 +0300

    update

--- dwm-6.5/config.h
+++ dwm-6.5/config.h
@@ -19,7 +19,7 @@ static const char col_gray1[]       = "#212335";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#555D69";
+static const char col_cyan[]        = "#81a1c1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
