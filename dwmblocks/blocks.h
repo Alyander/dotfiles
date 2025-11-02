fc81b975cba0695639754f08546003507c4fe21c
Alyander <momoroot33@gmail.com>
2025-11-02 20:39:28 +0200

    update

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,11 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/  
+	{"󰓃:", "amixer sget Master | awk -F'[][]' '/Left:/ { print $2 }'",					1,		20},
+  {" :", "xkblayout-state print '%s'",					1,		30},
+  {"", "date '+%d/%m/%Y %H:%M'",					5,		0},
+ };
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
