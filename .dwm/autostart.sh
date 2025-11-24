d200087ca4cc595811222513aa470534f226d79b
Alyander <momoroot33@gmail.com>
2025-11-24 19:58:05 +0200

    caps lock like ctrl

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,6 @@
 picom &
 feh --bg-fill /home/anatolii/walls/pixelcity.png &
-pulseaudio  --start
-setxkbmap -model pc104 -layout us,ru,ua -option grp:alt_shift_toggle &
+pulseaudio  --start &
+setxkbmap -model pc104 -layout us,ru,ua -option grp:alt_shift_toggle
+setxkbmap -option ctrl:nocaps
 dwmblocks &
