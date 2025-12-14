051ac7e69b45a28558840927f1b99f167b3afb55
Alaynder <momoroot33@gmail.com>
2025-12-14 21:34:32 +0200

    update for notebook

--- dwm-6.6/config.h
+++ dwm-6.6/config.h
@@ -13,7 +13,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetbrainsMono Nerd Font:size=11" };
+static const char *fonts[]          = { "JetbrainsMono Nerd Font:size=9" };
 static const char dmenufont[]       = "JetbrainsMono Nerd Font:size=11";
 static const char col_gray1[]       = "#212335";
 static const char col_gray2[]       = "#444444";
--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,6 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/  
 	{"󰓃:", "amixer sget Master | awk -F'[][]' '/Left:/ { print $2 }'",					1,		20},
   {" :", "xkblayout-state print '%s'",					1,		30},
+  {"󰁹:", "cat /sys/class/power_supply/BAT0/capacity", 15, 0},
   {"", "date '+%d/%m/%Y %H:%M'",					5,		0},
  };
 
