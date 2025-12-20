237fed66bb960a441b7fd80f4394ef684f13f560
Alyander <momoroot33@gmail.com>
2025-12-20 20:58:04 +0200

    resolved problem with neovim

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,26 +1,24 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    config = function ()
-        require'nvim-treesitter.configs'.setup {
-            ensure_installed = { "c", "markdown", "javascript", "html", "css", "typescript", "java", "python", "cpp", "bash", "sql"},
-              highlight = {
-    enable = true,              -- включить подсветку
-    disable = {},               -- можно добавить типы файлов, для которых отключить подсветку
-    additional_vim_regex_highlighting = false,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn", -- выбрать начальный узел
-      node_incremental = "grn", -- увеличивать узел
-      scope_incremental = "grc", -- увеличивать область
-      node_decremental = "grm", -- уменьшать узел
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "c", "markdown", "javascript", "html", "css",
+      "typescript", "java", "python", "cpp", "bash", "sql",
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
     },
   },
-sync = false,        -- Отключаем синхронное выполнение
-  context_commentstring = {
-    enable = true,
-  },
-        }
-    end
 }
+
