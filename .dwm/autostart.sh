febe2c3d7ea9fd99b009d5f8466f6fa43df4e9f2
alyander <momoroot33@gmail.com>
2025-12-20 21:16:45 +0200

    notebook update!

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,5 @@
 picom &
-feh --bg-fill /home/anatolii/walls/pixelcity.png &
+feh --bg-fill /home/anatolii/walls/pixelcity_snow.png &
 pulseaudio  --start &
 setxkbmap -model pc104 -layout us,ru,ua -option grp:alt_shift_toggle
 setxkbmap -option ctrl:nocaps
